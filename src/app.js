2ca5c5f875cf685aee490c960900ad5646d857c6
himan1009 <shishusha922@gmail.com>
2024-04-06 19:28:19 +0530

    live

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
-
 const app=express()
 
 app.use(cors({
@@ -12,7 +11,25 @@ app.use(cors({
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(express.static)
+app.use(express.static("public"))
 app.use(cookieParser())
+
+
+// routes import 
+
+import userRouter from'./routes/user.routes.js'
+
+
+
+
+
+// routes declaration
+
+app.use("/api/v1/users",userRouter)
+
+
+
+
+
 
 export {app}
